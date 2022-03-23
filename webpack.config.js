e0cc52b4bb6e134a153b08a63e89c4319637fdd2
Have Samuel <samiehave9@gmail.com>
2022-03-23 04:09:05 +0300

    Added different files to the package.json and webpack config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,24 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-module.exports = {
-  entry: './src/index.js',
+ const HtmlWebpackPlugin = require('html-webpack-plugin');
+  module.exports = { 
     mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-      static: './dist',
-    },
-plugins: [
-  new HtmlWebpackPlugin({
-
-    template: './src/index.html'
-  }),
-],
-  output: {
-    filename: '[name].bundles.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
- };
-
-
+     entry: { index: './src/index.js',
+     }, 
+     devtool: 'inline-source-map'
+     , devServer: { static: './dist',
+     },
+      plugins: [ new HtmlWebpackPlugin({
+         template: './src/index.html',
+         }),
+         ], 
+         output: { filename: '[name].bundle.js',
+          path: path.resolve(__dirname, 'dist'),
+           clean: true,
+           },
+            module: { rules: [ { test: /\.css$/i,
+             use: ['style-loader',
+              'css-loader'],
+            },
+          ],
+        },
+      }; 
